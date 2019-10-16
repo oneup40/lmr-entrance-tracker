ac103bb3721f6e247b7ce18ce64a3db239d19d98
oneup <oneup@arch-vm>
2019-10-16 13:41:22 -0400

    make ladder selector generic

--- src/components/LMREntranceTracker.vue
+++ src/components/LMREntranceTracker.vue
@@ -1,25 +1,80 @@
 <template>
     <div class="tracker-top">
         <LMRGateTracker/>
-        <LMRLadderTracker/>
+        <LMRPairTracker title="Ladders" :pairs="ladderPairs"/>
         <LMRDoorTracker/>
     </div>
 </template>
 
 <script>
 import LMRGateTracker from './LMRGateTracker.vue'
-import LMRLadderTracker from './LMRLadderTracker.vue'
 import LMRDoorTracker from './LMRDoorTracker.vue'
+import LMRPairTracker from './LMRPairTracker.vue'
 
 export default {
     name: 'LMREntranceTracker',
     components: {
         LMRGateTracker,
-        LMRLadderTracker,
-        LMRDoorTracker
+        LMRDoorTracker,
+        LMRPairTracker
     },
     data: () => ({
-    })
+        ladders: {
+            up: [
+                "Guidance (Shuriken)",
+                "Mausoleum (Top)",
+                "TotS (Top)",
+                "Inferno Lower (Map)",
+                "Inferno Upper (Spikes)",
+                "Extinction (Main)",
+                "Extinction (Palenque)",
+                "Extinction (Magatama)",
+                "TL Left (Closed)",
+                "TL Middle (Poison)",
+                "TL Right (Crystal Skull)",
+                "Endless (Philosopher)",
+                "Shrine Entrance (Top)",
+                "Graveyard Left (Hot Spring)",
+                "Gravevard Right (Lamp Refill)",
+                "Moonlight Left (Coin Chest)",
+                "Moonlight Right (Falling Block Trap)",
+                "Goddess (Lamp Refill)",
+                "Birth (Backside Door)",
+                "Mausoleum T (Top)",
+            ],
+            down: [
+                { name: "Surface Left", used: false },
+                { name: "Surface Right", used: false },
+                { name: "Guidance Left (Guild Shop)", used: false },
+                { name: "Guidance Right (Ankh Jewel)", used: false },
+                { name: "Mausoleum (Ankh Switch)", used: false },
+                { name: "Spring", used: false },
+                { name: "TL Left (Fairy Point)", used: false },
+                { name: "TL Right (Broken Elevator)", used: false },
+                { name: "Endless (Bottom)", used: false },
+                { name: "Shrine Main (Elevator)", used: false },
+                { name: "Shrine A (Unblocked Annex)", used: false },
+                { name: "Shrine B (Blocked Annex)", used: false },
+                { name: "Illusion Left (One Way Doors)", used: false },
+                { name: "Illusion Right (Eden)", used: false },
+                { name: "Graveyard (Kamitachi)", used: false },
+                { name: "Goddess (Shield Trap)", used: false },
+                { name: "Birth (Grail Tablet)", used: false },
+                { name: "DC (Entrance)", used: false },
+                { name: "Guidance T", used: false },
+                { name: "Mausoleum T (Secret)", used: false },
+            ]
+        }
+    }),
+
+    computed: {
+        ladderPairs: function() {
+            return {
+                first: this.$data.ladders.up,
+                second: this.$data.ladders.down
+            }
+        }
+    }
 }
 </script>
 
---
+++ src/components/LMRPairTracker.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <div><h3>{{title}}</h3></div>
+        <div class="lmr-pairs-top">
+            <template v-for="(_, ndx) of pairs.first">
+                <div class="lmr-pairs-row-first">
+                    {{pairs.first[ndx]}}
+                </div>
+                <div>
+                    <LMRPairSelector :entries="pairs.second" />
+                </div>
+                <div class="lmr-pairs-spacer"/>
+                <div class="lmr-pairs-row-second"
+                    v-bind:class="{used: pairs.second[ndx].used}"
+                >
+                    {{pairs.second[ndx].name}}
+                </div>
+            </template>
+        </div>
+    </div>
+</template>
+
+<script>
+import LMRPairSelector from './LMRPairSelector.vue'
+
+export default {
+    name: 'LMRPairTracker',
+    components: {
+        LMRPairSelector
+    },
+    props: {
+        title: String,
+        pairs: Object
+    },
+    data: () => ({
+    })
+}
+</script>
+
+<style>
+.lmr-pairs-top {
+    display: grid;
+    grid-template-columns: auto auto 1fr auto;
+    column-gap: 1.5em;
+}
+
+.lmr-pairs-row-first {
+    text-align: right;
+    overflow: hidden;
+}
+
+.lmr-pairs-row-second {
+    text-align: left;
+}
+
+.lmr-pairs-row-second.used {
+    text-decoration: line-through;
+    color: darkgray;
+}
+
+.lmr-pairs-spacer {
+    flex-grow: 1;
+}
+</style>
